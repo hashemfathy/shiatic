a28ec542d3275321ec1a9796613b24f39b812a61
hashem fathy <hashempoppo23@gmail.com>
2020-02-15 02:52:39 +0200

    make it better

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Client;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +10,9 @@ class DashboardController extends Controller
 {
   public function index()
   {
-    return view('backend.pages.dashboard.index');
+    $totalClientCount = Client::all()->count();
+    $maleClientCount = Client::where('gender', 'male')->count();
+    $femaleClientCount = Client::where('gender', 'female')->count();
+    return view('backend.pages.dashboard.index', compact('totalClientCount', 'maleClientCount', 'femaleClientCount'));
   }
 }
