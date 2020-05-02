b8ff6fdf7d4678bf4e0539fae496e0b32aa9dce8
hashem fathy <hashem.fathy@code95.info>
2020-05-02 05:03:32 +0200

    make it better

--- app/Http/Controllers/Admin/FinanceController.php
+++ app/Http/Controllers/Admin/FinanceController.php
@@ -11,6 +11,7 @@ use App\Specialist;
 use App\Visit;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FinanceController extends Controller
 {
@@ -32,6 +33,10 @@ class FinanceController extends Controller
             'monthVisitsIncome' => Visit::whereMonth('date', '=', Carbon::now()->month)->whereYear('date', '=', Carbon::now()->year)->sum('price'),
 
         ];
-        return view('backend.pages.finance.index', compact('income'));
+        $total =  DB::table('visits')
+            ->whereDate('date', '=', date("Y-m-d"))->sum('price');
+
+        return $total;
+        // return view('backend.pages.finance.index', compact('income'));
     }
 }
