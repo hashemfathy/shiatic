5ed6fc8c65fa4985d7fc8ef142ed31a900eff5bd
hashem fathy <hashem.fathy@code95.info>
2020-06-17 23:24:18 +0200

    make it better

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -17,18 +17,18 @@ class DashboardController extends Controller
     $maleClientCount = Client::where('gender', 'male')->count();
     $femaleClientCount = Client::where('gender', 'female')->count();
     $monthVisits = [];
-    $janVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 01 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $febVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 02 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $marVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 03 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $aprVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 04 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $mayVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 05 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $junVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 06 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $julVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 07 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $augVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 08 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $sepVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 09 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $octVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 10 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $novVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 11 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
-    $decVisits = collect(DB::select("SELECT * FROM visits WHERE EXTRACT(MONTH FROM date) = 12 AND EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE) "));
+    $janVisits = [];
+    $febVisits = [];
+    $marVisits = [];
+    $aprVisits = [];
+    $mayVisits = [];
+    $junVisits = [];
+    $julVisits = [];
+    $augVisits = [];
+    $sepVisits = [];
+    $octVisits = [];
+    $novVisits = [];
+    $decVisits = [];
     // ------
     $visitsCount = [
       'totalVisitsCount' => Visit::all()->count(),
@@ -49,66 +49,66 @@ class DashboardController extends Controller
         array_push($monthVisits, $visit);
       }
     }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 1 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($janVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 2 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($febVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 3 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($marVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 4 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($aprVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 5 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($mayVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 6 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($junVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 7 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($julVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 8 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($augVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 9 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($sepVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 10 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($octVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 11 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($novVisits, $visit);
-    //   }
-    // }
-    // foreach (Visit::all() as $visit) {
-    //   if (date("m", strtotime($visit->date)) == 12 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
-    //     array_push($decVisits, $visit);
-    //   }
-    // }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 1 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($janVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 2 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($febVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 3 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($marVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 4 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($aprVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 5 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($mayVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 6 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($junVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 7 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($julVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 8 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($augVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 9 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($sepVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 10 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($octVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 11 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($novVisits, $visit);
+      }
+    }
+    foreach (Visit::all() as $visit) {
+      if (date("m", strtotime($visit->date)) == 12 && date("Y", strtotime($visit->date)) == Carbon::now()->year) {
+        array_push($decVisits, $visit);
+      }
+    }
     $monthVisitsCount = [
       'totalVisitsCount' => collect($monthVisits)->count(),
       'ahmedAdelVisitsCount' => collect($monthVisits)->where('specialist_id', 2)->count(),
@@ -124,18 +124,18 @@ class DashboardController extends Controller
       'unselectedVisitsCount' => collect($monthVisits)->where('specialist_id', 1)->count(),
     ];
     $statics = [
-      'janVisits' => $janVisits->count(),
-      'febVisits' => $febVisits->count(),
-      'marVisits' => $marVisits->count(),
-      'aprVisits' => $aprVisits->count(),
-      'mayVisits' => $mayVisits->count(),
-      'junVisits' => $junVisits->count(),
-      'julVisits' => $julVisits->count(),
-      'augVisits' => $augVisits->count(),
-      'sepVisits' => $sepVisits->count(),
-      'octVisits' => $octVisits->count(),
-      'novVisits' => $novVisits->count(),
-      'decVisits' => $decVisits->count(),
+      'janVisits' => collect($janVisits)->count(),
+      'febVisits' => collect($febVisits)->count(),
+      'marVisits' => collect($marVisits)->count(),
+      'aprVisits' => collect($aprVisits)->count(),
+      'mayVisits' => collect($mayVisits)->count(),
+      'junVisits' => collect($junVisits)->count(),
+      'julVisits' => collect($julVisits)->count(),
+      'augVisits' => collect($augVisits)->count(),
+      'sepVisits' => collect($sepVisits)->count(),
+      'octVisits' => collect($octVisits)->count(),
+      'novVisits' => collect($novVisits)->count(),
+      'decVisits' => collect($decVisits)->count(),
     ];
     return view('backend.pages.dashboard.index', compact('totalClientCount', 'maleClientCount', 'femaleClientCount', 'visitsCount', 'monthVisitsCount', 'statics'));
   }
