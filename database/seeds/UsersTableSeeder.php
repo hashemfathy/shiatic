ce5b36557ff3dee6316bf212788070fa8ece0edd
hashem fathy <hashem.fathy@code95.info>
2020-08-26 00:53:37 +0200

    make it better

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,8 +16,8 @@ class UsersTableSeeder extends Seeder
         \DB::table('users')->truncate();
         // factory(User::class)->create();
         $users = [
-            ['id' => 1, 'name' => 'ahmed adel', 'email' => 'ahmed_adel@shiatic.com', 'email_verified_at' => now(), 'password' => bcrypt('Shi@tic'), 'is_admin' => true, 'remember_token' => Str::random(10),],
-            ['id' => 2, 'name' => 'admin', 'email' => 'admin@shiatic.com', 'email_verified_at' => now(), 'password' => bcrypt('password'), 'is_admin' => true, 'remember_token' => Str::random(10),],
+            ['id' => 1, 'name' => 'ahmed adel', 'email' => 'ahmed_adel@shiatic.com', 'email_verified_at' => now(), 'password' => bcrypt('Shi@tic'), 'is_admin' => 1, 'remember_token' => Str::random(10),],
+            ['id' => 2, 'name' => 'admin', 'email' => 'admin@shiatic.com', 'email_verified_at' => now(), 'password' => bcrypt('password'), 'is_admin' => 2, 'remember_token' => Str::random(10),],
         ];
         foreach ($users as $user) {
             User::create($user);
